e0840c566fbcd98389a3394cefbf3f10e285f4ca
Maria Eleni Kostopoulou <marialena.kostopoulou@outlook.com>
2022-04-23 01:27:43 +0200

    entailement_v2

--- entailement.py
+++ entailement.py
@@ -22,64 +22,66 @@ class Resolution:
             clauses.append(cnf_b)
 
         # contradiction ¬φ
-        cnf_frm = to_cnf(formula)
-        clauses.append(~cnf_frm)
+        cnf_frm = to_cnf(~to_cnf(formula))
+        clauses.append(cnf_frm)
 
         # Apply resolution to resulting clauses
         repeat = True
+        temp_clauses = []
         clauses_to_be_resolved = clauses
+        resolved = []
         while repeat:
-            previous = clauses_to_be_resolved             
+            # unique combinations of clauses
             list_pairs = self.pair_maker(clauses_to_be_resolved)
+            # print(list_pairs)
+
+            # compare literals of all pairs
             repeat = False
-            clauses_to_be_resolved = []
             for clause1, clause2 in list_pairs:
-                left_overs, rerun = self.resolve_complementary(clause1, clause2)
-                
-                while rerun>0:
-                    list_pairs = self.pair_maker(left_overs)
-                    for clause1, clause2 in list_pairs:
-                        left_overs, rerun = self.resolve_complementary(clause1, clause2)
-                    
-                
-                
-                if len(left_overs)>0:
-                        c = Or(*tuple(left_overs))
-                if c not in clauses_to_be_resolved:
-                    clauses_to_be_resolved.append(c)
-            # Check if no new clause has been added to the KB
-            if previous == clauses_to_be_resolved:
-                return False
+                c1_literals = list(clause1.args) if len(list(clause1.args)) > 1 else [clause1]
+                c2_literals = list(clause2.args) if len(list(clause2.args)) > 1 else [clause2]
+
+                # resolve complementary literals, if any
+                left_overs = self.resolve_complementary(c1_literals, c2_literals)
+
+                if left_overs is not None:
+                    # every time we produce a new clause we will start resolving again
+                    # until no new clause is created or the set is empty
+                    repeat = True
+                    c = Or(*tuple(left_overs))
+                    if c not in temp_clauses:
+                        temp_clauses.append(c)
+                    if clause1 not in resolved:
+                        resolved.append(clause1)
+                    if clause2 not in resolved:
+                        resolved.append(clause2)
+
+            if False in temp_clauses:
+                return True
+
+            clauses_to_be_resolved = list(set(clauses_to_be_resolved).difference(set(resolved)) | set(temp_clauses))
+
+            if repeat:
+                temp_clauses = []
+
         isEmptyClause = not len(clauses_to_be_resolved) > 0
-        # sys.exit()
         return isEmptyClause
     
-    def resolve_complementary (self, clause1, clause2):
-        rerun = 0
-        left_overs = []
-        c1_literals = self.agm.Belief_sub(to_cnf(clause1))
-        c2_literals = self.agm.Belief_sub(to_cnf(clause2))
-        for literal_c1 in c1_literals:
-            for literal_c2 in c2_literals:
-                if literal_c1 == ~literal_c2 or literal_c2 == ~literal_c1:
-                    repeat = True
+    def resolve_complementary(self, c1_literals, c2_literals):
+        updated = False
+        literals = c1_literals + c2_literals
+        for i in literals:
+            if ~i in literals:
+                updated = True
+                literals.remove(i)
+                literals.remove(~i)
 
-                    c1_literals.remove(literal_c1)
-                    c2_literals.remove(literal_c2)
+        return list(set(literals)) if updated else None
 
-                    left_overs = list(set(c1_literals + c2_literals))
-                    
-        for i in left_overs:
-            if ~(i) in left_overs:
-                rerun = 1
-                    
-        return left_overs, rerun
-    
     def pair_maker (self, clauses):
         # unique combinations of clauses
         pairs = itertools.combinations(clauses, 2)
         list_pairs = list(pairs)
-        print("clauses_to_be_resolved: ", clauses)
         return list_pairs
         
         
@@ -87,12 +89,22 @@ class Resolution:
 
 if __name__ == "__main__":
     res = Resolution()
+    # example
+    Belief_Set = res.check_entailment(['~p >> q'], 'p | q')
+    print(Belief_Set)
+
     # example from slides9 page 48/73
     Belief_Set = res.check_entailment(['~r | p | s', '~p | r', '~s | r', '~r'], '~p')
     print(Belief_Set)
 
     # example from ex9 1.
-    #Belief_Set = res.check_entailment(['~p >> q', 'q >> p', 'p >> r', 'r & s'], 'p & r & s')
-    #print(Belief_Set)
+    Belief_Set = res.check_entailment(['~p >> q', 'q >> p', 'p >> r', 'r & s'], 'p & r & s')
+    print(Belief_Set)
 
-    # print("Belief_Set  : ", output(agm.belief_base.belief_set))
+    # example from ex9 1. wrong
+    Belief_Set = res.check_entailment(['~p >> q', 'q >> p', 'p >> r', 'r & s'], 'p & r & u')
+    print(Belief_Set)
+
+    # example from ex9 1. wrong
+    Belief_Set = res.check_entailment(['~p >> q'], 'q')
+    print(Belief_Set)
